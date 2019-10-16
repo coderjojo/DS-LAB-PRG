13ae8bafaeec64ee833994ccc6d3f3b74f429511
coderjojo <45591748+coderjojo@users.noreply.github.com>
2019-10-16 19:20:53 +0530

    Modify Added
    Delete operation remaining

--- Assignment3.cpp
+++ Assignment3.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 struct Student
@@ -66,28 +66,76 @@ void SearchDetails(struct Student S[], int n){
 	{
 		if(S[i].roll_no == key){
 			flag = 1;
-			cout<<"Name of student is: "<<S[i].name;
+			cout<<"Name of student is: "<<S[i].name<<endl;
 			break;
 		}
 
 	}
 
 	if(!flag)
-		cout<<"\nTry again with correct roll number / roll number not exist!";
+		cout<<"\nTry again with correct roll number / roll number not exist!"<<endl;
 
 }
+
+
+void ChangeDetail(struct Student S[], int n){
+	cout<<"\nEnter the student roll No:";
+	int key;
+	cin>>key;
+	
+	cout<<"Enter new Data: ";
+	for(int i = 0; i < n; i++){
+		if(S[i].roll_no == S[key-1].roll_no){
+			cout<<"Change name: ";
+			cin>>S[i].name;
+			cout<<"Change Address: ";
+			cin>>S[i].address;
+		}
+   }
+ }
+ 
+// void DeleteE(struct Student S[], int n){
+
+//}
 
 
 
 int main(){
 
-	int n;
-	cout<<"Enter the name of the Student: ";
+	int n, ch=1;
+	cout<<"Enter the number of the Student: ";
 	cin>>n;
 	Student S[n];
 
 	Accept(S,n);
+	do{
+	cout<<"\nEnter choice: \n1.SortDetails\n2.Display\n3.SearchDetails\n4.ChangeDetails\n5.Delete Record\n6.Enter 0 to exit! ";
+	cin>>ch;
+	
+	switch(ch){
+	
+	case 1:
 	SortDetails(S,n);
+	break;
+	
+	case 2:
 	Display(S,n);
+	break;
+	
+	case 3:
 	SearchDetails(S,n);
+	break;
+	
+	case 4:
+	ChangeDetail(S,n);
+	break;
+	
+	//case 5: 
+	//DeleteE(S,n);
+	
+	default:
+	cout<<"\nEnter right choice: ";
+	break;
+	}
+ }while(ch!=0);
 }
