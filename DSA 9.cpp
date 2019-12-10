dc8ec47ce2d19d32549f279c400fe75762c26fe0
coderjojo <45591748+coderjojo@users.noreply.github.com>
2019-12-10 18:50:24 +0530

    Updated Infinix to postfix

--- DSA 9.cpp
+++ DSA 9.cpp
@@ -1,71 +1,53 @@
 #include<iostream>
-
-# define max 100
-char st[max];
-int top=-1;
 using namespace std;
-void push(char ele)
-{
-  st[++top]=ele;
-}
-char pop()
-{
-   return (st[top--]);
-}
-int priorty(char ch)
-{
-  switch(ch)
-  {
+char stack[20];
+int top = -1;
 
-   case '^' : return 4;
-   case '*' : return 3;
-   case '/' : return 3;
-   case '+' : return 2;
-   case '-' : return 2;
-   case '(' : return 1;
-   }
- }
- int main()
- {
-   char infix[50],postfix[50],ch,element;
-   int i=0,k=0;
-   cout<<"enter the infix notation: ";
-   cin>>infix;
-   for(i=0;infix[i]!='\0';i++)
-   {
-    ch=infix[i];
-
-      if(isalnum(ch))
-     {
-       postfix[k++]=ch;
-     }
-      else if(ch=='(')
-     {
-       push(ch);
-     }
-     else if(ch==')')
-
-     {
-          while(st[top]!='(')
-             postfix[k++]=pop();
-       element=pop();
-       }
-     else
-     {
-       while(priorty(ch)<=priorty(st[top]))
-       {
-         postfix[k++]=pop();
-        }
-       push(ch);
-     }
-    }
-    while(st[top]>=0)
-         postfix[k++]=pop();
-    postfix[k++]='\0';
-    cout<<"postfix="<<postfix;
-    return 0;
+char pop(){
+	if(top == -1)
+		return -1;
+	else
+		return stack[top--];
 }
 
+void push(char x){
+	stack[++top] = x;
+}
 
+int priority(char c){
 
+	if(c == '(') return 1;
+	else if(c == '+' || c == '-') return 2;
+	else if(c == '/' || c == '*') return 3;
+	else if(c == '^') return 4;
+	else return -1;
+}
 
+int main(){
+	char inf[20], post[20], *e, s;
+
+	cout<<"Enter the infix notation: ";
+	cin>>inf;
+	e = inf;
+	while(*e != '\0'){
+		if(isalnum(*e))
+			cout<<*e;
+		else if(*e == '(')
+			push(*e);
+		else if(*e == ')'){
+			while((s = pop()) != '(' )
+				cout<<s;
+		}
+
+		else{
+			while(priority(stack[top]) >= priority(*e))
+				cout<<pop();
+			push(*e);
+		}
+		e++;
+	}
+
+	while(top != -1)
+		cout<<pop();
+}
+		
